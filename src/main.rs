25aa60dac8fe62af516a2928bdcc508b4bc669d1
Christoph Doblander <chris@degree.at>
2020-11-23 22:03:10 +0100

    updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate indicatif;
 extern crate clap;
 extern crate crossbeam;
 extern crate ring;
+extern crate num_cpus;
 
 use std::fmt;
 use std::collections::HashMap;
@@ -10,21 +11,20 @@ use std::io::{BufReader, Result, Read, Write, BufWriter};
 use std::ffi::OsString;
 
 use std::fs::{self};
-use std::path::{Path, PathBuf};
+use std::path::{PathBuf};
 use std::sync::{Mutex, Arc};
-use std::time::Duration;
 
 use crossbeam::channel::Sender;
 use crossbeam::channel::unbounded;
 use crossbeam::thread;
 
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 use ring::digest::{Context, Digest, SHA256};
 
 use data_encoding::HEXUPPER;
 
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 
 use rand::seq::SliceRandom;
 
@@ -48,7 +48,7 @@ fn sha256_digest(path: PathBuf) -> Result<Digest> {
     let mut reader = BufReader::new(input);
 
     let mut context = Context::new(&SHA256);
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 8*1024];
 
     loop {
         let count = reader.read(&mut buffer)?;
@@ -66,15 +66,15 @@ struct HashTask {
     path: PathBuf,
 }
 
-fn visit_dirs(path_sender: Sender<Option<HashTask>>, side: DirSide, dir: &Path) -> Result<()> {
+fn visit_dirs(path_sender: Sender<Option<HashTask>>, side: DirSide, dir: PathBuf) -> Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path_loc = entry.path();
             if path_loc.is_dir() {
-                visit_dirs(path_sender.clone(), side, path_loc.as_path())?;
+                visit_dirs(path_sender.clone(), side, path_loc)?;
             } else {
-                path_sender.send(Some(HashTask { side: side, path: entry.path() })).unwrap();
+                path_sender.send(Some(HashTask { side, path: entry.path() })).unwrap();
             }
         }
     }
@@ -92,7 +92,7 @@ fn write_missing(matching: &HashMap<OsString, HashResult>, side: DirSide) -> std
     let mut buf_writer_missing = BufWriter::new(missing_left);
     matching.iter()
         .filter(|&(_k, v)| v.side != side)
-        .for_each(|(_k, v)| { writeln!(&mut buf_writer_missing, "missing {}: {}", side, v.path.display()).unwrap() }); //TODO! write errors are not handled
+        .for_each(|(_k, v)| { writeln!(&mut buf_writer_missing, "missing {}: {}", side, v.path.display()).unwrap() });
     buf_writer_missing.flush()?;
 
     Ok(())
@@ -113,10 +113,8 @@ fn write_results(matching: &HashMap<OsString, HashResult>, missmatches: Vec<(Has
     }
     buf_writer_mismatch_file.flush()?;
 
-
     write_missing(matching, DirSide::Left)?;
     write_missing(matching, DirSide::Right)?;
-
 
     Ok(())
 }
@@ -127,48 +125,16 @@ struct HashResult {
     hash: String,
 }
 
-fn main() {
-    let matches = App::new("Parallel directory compare")
-        .version("0.0.1")
-        .author("Christoph Doblander")
-        .about("Compares the content of two directories")
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
-        .arg(Arg::with_name("left")
-            .short("l")
-            .long("left")
-            .value_name("LEFT_DIRECTORY")
-            .help("Sets the left directory")
-            .takes_value(true))
-        .arg(Arg::with_name("right")
-            .short("r")
-            .long("right")
-            .value_name("RIGHT_DIRECTORY")
-            .help("Sets the right directory")
-            .takes_value(true))
-        .arg(Arg::with_name("cores")
-            .short("t")
-            .long("threads")
-            .value_name("THREAD_COUNT")
-            .help("Sets how many threads should be used for hashing")
-            .takes_value(true))
-        .get_matches();
+struct Config {
+    left: PathBuf,
+    right: PathBuf,
+    total_threads: i32,
+}
 
-    let mut verbose = matches.is_present("v");
+fn start_comparing(cfg: Config) {
+    let thread_count = cfg.total_threads;
+    let source_paths = vec![(DirSide::Left, cfg.left.clone()), (DirSide::Right, cfg.right.clone())];
 
-    let mut dir_left: String = String::from(matches.value_of("LEFT_DIRECTORY").unwrap());
-    let mut dir_right: String = String::from(matches.value_of("RIGHT_DIRECTORY").unwrap());
-    let mut total_threads: i32 = matches.value_of("THREAD_COUNT").unwrap().parse().unwrap();
-
-
-    let str_dir_left = dir_left.as_str();
-    let str_dir_right = dir_right.as_str();
-
-    let source_paths = vec![(DirSide::Left, Path::new(str_dir_left)), (DirSide::Right, Path::new(str_dir_right))];
-
-    //Scanning ot directories
     thread::scope(|s| {
         let walking_finished = Arc::new(Mutex::new(0));
         let (tx_walked_paths, rx_walked_paths) = unbounded();
@@ -184,7 +150,7 @@ fn main() {
                 let mut num = walking_finished_cnt.lock().unwrap();
                 *num += 1;
                 if *num > 1 {
-                    send_cancellation_tokens(tx_second, total_threads);
+                    send_cancellation_tokens(tx_second, thread_count);
                 }
             });
         }
@@ -204,19 +170,19 @@ fn main() {
             all_hashtasks.shuffle(&mut rng);
 
             println!("Start comparing files:");
-            tx_max_count.send(all_hashtasks.len() as u64);
+            tx_max_count.send(all_hashtasks.len() as u64).unwrap();
 
             //Send
             while let Some(ht) = all_hashtasks.pop() {
-                tx_paths_to_check.send(Some(ht));
+                tx_paths_to_check.send(Some(ht)).unwrap();
             }
 
             //Send CancellationToken
-            tx_paths_to_check.send(None);
+            tx_paths_to_check.send(None).unwrap();
         });
 
         let (tx_hashresults, rx_resultmerger) = unbounded();
-        for _ in 0..total_threads {
+        for _ in 0..thread_count {
             let rx_first = receiver_paths_to_check.clone();
             let tx_first = tx_hashresults.clone();
             s.spawn(move |_| {
@@ -246,34 +212,29 @@ fn main() {
                 match o_hr {
                     Some(hr) => {
                         let rel_path = match hr.side {
-                            DirSide::Left => hr.path.strip_prefix(str_dir_left),
-                            DirSide::Right => hr.path.strip_prefix(str_dir_right),
+                            DirSide::Left => hr.path.strip_prefix(cfg.left.to_str().unwrap()),
+                            DirSide::Right => hr.path.strip_prefix(cfg.right.to_str().unwrap()),
                         };
 
-                        match rel_path {
-                            Ok(path) => {
-                                let path_os = path.as_os_str().to_os_string();
-                                match matching.get(&path_os) {
-                                    Some(_hr_other) => { //It's already one time here
-                                        let hr_other_side = matching.remove(&path_os).unwrap();
-                                        if hr_other_side.hash != hr.hash {
-                                            notmatched.push((hr, hr_other_side));
-                                        }
-                                    }
-                                    None => {
-                                        matching.insert(path_os, hr);
+                        if let Ok(path) = rel_path {
+                            let path_os = path.as_os_str().to_os_string();
+                            match matching.get(&path_os) {
+                                Some(_hr_other) => { //It's already one time here
+                                    let hr_other_side = matching.remove(&path_os).unwrap();
+                                    if hr_other_side.hash != hr.hash {
+                                        notmatched.push((hr, hr_other_side));
                                     }
                                 }
-                            }
-                            Err(_) => {
-                                todo!();
+                                None => {
+                                    matching.insert(path_os, hr);
+                                }
                             }
                         }
                         pb.inc(1);
                     }
                     None => {
                         cnt_cancellation_tokens_received += 1;
-                        if cnt_cancellation_tokens_received > total_threads {
+                        if cnt_cancellation_tokens_received > cfg.total_threads {
                             pb.finish_with_message("writing results...");
                             if matching.len() == 0 && notmatched.len() == 0 {
                                 println!("Directories exactly the same!");
@@ -290,5 +251,57 @@ fn main() {
                 }
             }
         });
-    }).unwrap();
+    }).unwrap()
+}
+
+fn main() {
+    let matches = App::new("Parallel directory compare")
+        .version("0.0.1")
+        .author("Christoph Doblander")
+        .about("Compares the content of two directories")
+        .arg(Arg::with_name("LEFT_DIRECTORY")
+            .short("l")
+            .long("left")
+            .help("Sets the left directory")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("RIGHT_DIRECTORY")
+            .short("r")
+            .long("right")
+            .help("Sets the right directory")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("THREAD_COUNT")
+            .short("t")
+            .long("threads")
+            .help("Sets how many threads should be used for hashing")
+            .takes_value(true))
+        .get_matches();
+
+    let dir_left: String = String::from(matches.value_of("LEFT_DIRECTORY").unwrap());
+    let left = PathBuf::from(dir_left.as_str());
+    if !left.exists() {
+        eprintln!("Left directory does not exist");
+        return;
+    }
+
+    let dir_right: String = String::from(matches.value_of("RIGHT_DIRECTORY").unwrap());
+    let right = PathBuf::from(dir_right.as_str());
+    if !right.exists() {
+        eprintln!("Right directory does not exist");
+        return;
+    }
+
+    let mut total_threads: i32 = num_cpus::get() as i32;
+    if let Some(thread_count_str) = matches.value_of("THREAD_COUNT") {
+	    if let Ok(thread_count_int) = thread_count_str.parse::<i32>() {
+	        total_threads = thread_count_int;
+	    } else {
+            eprintln!("Could not parse thread count");
+            return;
+        }
+    }
+
+    let cfg = Config{left, right, total_threads };
+    start_comparing(cfg)
 }
